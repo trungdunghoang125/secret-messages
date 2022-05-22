ef91b10498da01bfd5bb467f78f4c55edf28ae6d
trungdunghoang <trungdunghoang125@gmail.com>
2022-05-22 16:29:40 +0700

    Initial commit

---
+++ app/src/main/java/com/trungdunghoang125/secretmessages/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.trungdunghoang125.secretmessages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
