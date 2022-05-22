f50dd561dda12c607a736ff23906652fb094749a
trungdunghoang <trungdunghoang125@gmail.com>
2022-05-22 17:38:06 +0700

    Add Welcome Fragment

---
+++ app/src/main/java/com/trungdunghoang125/secretmessages/WelcomeFragment.kt
@@ -0,0 +1,18 @@
+package com.trungdunghoang125.secretmessages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+}
