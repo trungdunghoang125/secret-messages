792d9c97042faf33aa185265975d905ce29e90af
trungdunghoang <trungdunghoang125@gmail.com>
2022-05-24 00:27:26 +0700

    Add message fragment and add navigation component to app

--- app/src/main/java/com/trungdunghoang125/secretmessages/WelcomeFragment.kt
+++ app/src/main/java/com/trungdunghoang125/secretmessages/WelcomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
 
@@ -13,6 +16,12 @@ class WelcomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton = view.findViewById<Button>(R.id.start)
+        startButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+        return view
     }
 }
